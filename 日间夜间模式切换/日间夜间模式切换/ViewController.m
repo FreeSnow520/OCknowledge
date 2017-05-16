248f76fd8fd585c42de276461ff804ad6118da33
FreeSnow520 <sxf987958774@163.com>
2017-05-16 16:14:43 +0800

    增加代码日夜间模式
    日夜间模式

--- 日间夜间模式切换/日间夜间模式切换/ViewController.m
+++ 日间夜间模式切换/日间夜间模式切换/ViewController.m
@@ -9,17 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    BOOL _isNight;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _isNight = YES;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
+- (IBAction)style:(id)sender {
+    if (_isNight == YES) {
+        _isNight = NO;
+        self.view.window.alpha = 0.4;
+    }else{
+        _isNight = YES;
+        self.view.window.alpha = 1.0;
+    }
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
